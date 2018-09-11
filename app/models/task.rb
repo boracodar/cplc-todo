bc2454e1d8fa29f9fc8ebcd0b44609e9e691cbd1
Lucas Caton <lucascaton@gmail.com>
2018-09-11 12:38:53 +1000

    Update 'tasks/_table_row' partial view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,28 @@
 class Task < ApplicationRecord
   validates :description, presence: true
   validates :done, inclusion: { in: [true, false] }
+
+  def symbol
+    case status
+    when 'pending' then '»'
+    when 'done'    then '✓'
+    when 'expired' then '✕'
+    end
+  end
+
+  def css_color
+    case status
+    when 'pending' then 'primary'
+    when 'done'    then 'success'
+    when 'expired' then 'danger'
+    end
+  end
+
+  private
+
+  def status
+    return 'done' if done?
+
+    due_date.past? ? 'expired' : 'pending'
+  end
 end
